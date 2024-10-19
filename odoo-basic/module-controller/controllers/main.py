80e299612436e191fadfecb014a81ba5ae94842e
TyLe <vanty0829@gmail.com>
2024-10-19 16:23:03 +0700

    a

--- odoo-basic/module-controller/controllers/main.py
+++ odoo-basic/module-controller/controllers/main.py
@@ -1,6 +1,40 @@
+# import json
+# import werkzeug
 from odoo import http
+from odoo.http import request
 
-class TyController(http.Controller):
-    @http.route('/ty',auth='public',type='http')
-    def ty_check(self):
-        return "Check check"
+
+class MountainController(http.Controller):
+
+    @http.route('/mountain', auth='public', type='http')
+    def mountain_check(self):
+        return "Mountain check check check"
+    
+    # @http.route(['/mountain','/mountain1'], auth='public', type='http')
+    # def mountain_check(self):
+    #     return "Mountain check check check"
+
+    # @http.route('/mountain/<int:id>', auth='public', type='http')
+    # def mountain_check(self, id):
+    #     return "Mountain check check check %s" % str(id)
+
+    # @http.route('/mountain', auth='public')
+    # def mountain_check(self):
+    #     return werkzeug.utils.redirect('https://www.google.com')
+
+    # @http.route('/mountain', auth='public')
+    # def mountain_check(self):
+    #     return request.render("web.login")
+
+    # @http.route('/mountain', auth='public', type='http')
+    # def mountain_check(self):
+    #     return json.dumps({
+    #         "check": "check 123"
+    #     })
+
+    # @http.route('/mountain', auth='public', type='http')
+    # def mountain_check(self):
+    #     partner = request.env['res.partner'].sudo().create({
+    #         'name' : 'Mountain'
+    #     })
+    #     return 'Partner has been created'
