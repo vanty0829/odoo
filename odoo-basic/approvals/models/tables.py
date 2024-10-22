e1f9b57892102e67870146ead51919afbabedc25
tyleitech <ty.le@itech.asia>
2024-10-22 17:25:02 +0700

    a

--- odoo-basic/approvals/models/tables.py
+++ odoo-basic/approvals/models/tables.py
@@ -1,15 +1,9 @@
 from odoo import api, fields, models, _
 
 
-class Player(models.Model):
-    _name = 'requests' # tển bảng
-    _description = 'Player'
+class Tables(models.Model):
+    _name = 'tables' # tển bảng
+    _description = 'tables'
 
     name = fields.Char(string='Name', required = True)
-    image = fields.Binary(string='Image', attachment = True)
-    country = fields.Char(string='Country', required = True)
-    gender = fields.Selection([('male','Male'),('female','Female')], string= 'Gender', default='male')
-    day_of_birth = fields.Datetime(string='Day of birth')
-    position =  fields.Char(string='Position',groups='module_category_player.group_player_manager')
-    height =  fields.Float(string='Height')
-    weight =  fields.Float(string='Weight')
+    schema_id = fields.Many2one('schemas', string='Schema')
