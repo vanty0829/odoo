afa7776aa2685afd8dc0cb75138df0173341ceeb
tyleitech <ty.le@itech.asia>
2024-10-23 17:27:52 +0700

    a

--- odoo-basic/approvals/models/requests.py
+++ odoo-basic/approvals/models/requests.py
@@ -1,5 +1,5 @@
 from odoo import api, fields, models, _
-
+import json
 
 class Requests(models.Model):
     _name = 'requests' # tển bảng
@@ -7,7 +7,30 @@ class Requests(models.Model):
     _rec_name = 'title'
     title = fields.Char(string='Title')
     description = fields.Char(string='Description')
+
     schema_id = fields.Many2many('schemas', string='Schema')
+
+    table_id_domain = fields.Char(
+       compute="_compute_table_id_domain",
+       readonly=True,
+       store=False,
+   )
+
+
+
+    @api.onchange("schema_id")
+    def _compute_table_id_domain(self):
+        domain = []
+        id = 0
+        for i in self.schema_id:
+            if id == 0:
+                domain = [('full_name','=like',f'{i.name}.%')]
+            else:
+                domain = ['|']+domain +[('full_name','=like',f'{i.name}.%')]
+            id = id + 1
+        # ['|',('full_name','=like','bronze.%'),('full_name','=like','silver.%')]
+        self.table_id_domain = str(domain)
+
     table_id = fields.Many2many('tables', string='Table')
     users = fields.Text(string='Users')
     
@@ -31,6 +54,9 @@ class Requests(models.Model):
     
     def action_submit(self):
         self.state = 'submitted'
+
+    def action_check(self):
+        pass
     # column_id = fields.Many2many('columns', string='Column')
     # image = fields.Binary(string='Image', attachment = True)
     # country = fields.Char(string='Country', required = True)
--- odoo-basic/approvals/models/tables.py
+++ odoo-basic/approvals/models/tables.py
@@ -7,7 +7,7 @@ class Tables(models.Model):
     _rec_name = 'full_name'
     name = fields.Char(string='Name', required = True)
     schema_id = fields.Many2one('schemas', string='Schema')
-    full_name = fields.Char(string='Full Name', compute='_compute_name')
+    full_name = fields.Char(string='Full Name', compute='_compute_name',store=True)
     schema_name = fields.Char(related='schema_id.name')
 
     def _compute_name(self):
