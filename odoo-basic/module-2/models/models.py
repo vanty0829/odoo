dee41fafb1e7f7f1aefa41b6024148b0fb7c4b57
TyLe <vanty0829@gmail.com>
2024-10-26 18:29:17 +0700

    a

--- odoo-basic/module-2/models/models.py
+++ odoo-basic/module-2/models/models.py
@@ -10,10 +10,6 @@ class Player(models.Model):
     country = fields.Char(string='Country', required = True)
     gender = fields.Selection([('male','Male'),('female','Female')], string= 'Gender', default='male')
     day_of_birth = fields.Datetime(string='Day of birth')
-<<<<<<< HEAD
-    position =  fields.Char(string='Position',groups='player.group_player_manager')
-=======
     position =  fields.Char(string='Position',groups='module_category_player.group_player_manager')
->>>>>>> b521d79daac144f916ad8cc00c68f032b5b86f18
     height =  fields.Float(string='Height')
     weight =  fields.Float(string='Weight')
