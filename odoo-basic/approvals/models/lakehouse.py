783d1c3be92a0e797955b184f6d9646781291cb9
tyleitech <ty.le@itech.asia>
2024-10-28 17:22:00 +0700

    a

--- odoo-basic/approvals/models/lakehouse.py
+++ odoo-basic/approvals/models/lakehouse.py
@@ -13,31 +13,37 @@ class Lakehouse(models.Model):
     user_id = fields.Many2many('ms_user', string='MS User')
 
 
-    def group_api(self):
+    def group_api(self,values):
         engine = create_engine('postgresql+psycopg2://odoo:odoo@localhost:5433/odoo')
-        user_list = pd.read_sql( 
-            f"SELECT ms_user_id id FROM public.lakehouse_ms_user_rel where lakehouse_id = {self.ids[0]}" , 
-            con=engine 
-        ).to_json(orient='records')
-        lista = [self.env['ms_user'].search([('id','=',i['id'])]).object_id for i in json.loads(user_list)]
-        listb = [i.object_id for i in self.user_id]
+        group_id = self.group_id if self.group_id else values['group_id']
+        if self.ids:
+            user_list = pd.read_sql( 
+                f"SELECT ms_user_id id FROM public.lakehouse_ms_user_rel where lakehouse_id = {self.ids[0]}" , 
+                con=engine 
+            ).to_json(orient='records')
+        
+            lista = [self.env['ms_user'].search([('id','=',i['id'])]).object_id for i in json.loads(user_list)]
+            listb = [i.object_id for i in self.user_id]
+        else:
+            lista = []
+            listb = [self.env['ms_user'].search([('id','=',i[-1])])['object_id'] for i in values['user_id']]
         addlist = list(set(listb) - set(lista))
         rmlist = list(set(lista) - set(listb))
 
         for i in addlist:
-            ms.group_add_member(self.group_id,i)
+            ms.group_add_member(group_id,i)
 
         for i in rmlist:
-            ms.group_remove_member(self.group_id,i)
+            ms.group_remove_member(group_id,i)
 
     @api.model
     def create(self, values):
+        self.group_api(values)
         res = super(Lakehouse, self).create(values)
-        self.group_api()
         return res
 
 
     def write(self, values):
         res = super(Lakehouse, self).write(values)
-        self.group_api()
+        self.group_api(values)
         return res
